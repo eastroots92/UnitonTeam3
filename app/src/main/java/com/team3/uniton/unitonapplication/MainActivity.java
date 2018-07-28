f2ef50a7ea85c2fe07582aee1e6a706ce949213d
Eastroots92 <eastroots92@gmail.com>
2018-07-28 17:39:00 +0900

    Fix Header scroll action
    Header scroll action시 헤더 고정을 취소한다.

--- app/src/main/java/com/team3/uniton/unitonapplication/MainActivity.java
+++ app/src/main/java/com/team3/uniton/unitonapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.team3.uniton.unitonapplication;
 
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
     }
 }
