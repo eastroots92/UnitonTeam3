dd2b9f410e8ed846f3e551d5ec9515b17dcb9352
Eastroots92 <eastroots92@gmail.com>
2018-07-28 19:56:49 +0900

    Set tutorial_three button listener

--- app/src/main/java/com/team3/uniton/unitonapplication/TutorialThreeFragment.java
+++ app/src/main/java/com/team3/uniton/unitonapplication/TutorialThreeFragment.java
@@ -1,43 +1,41 @@
 package com.team3.uniton.unitonapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-public class TutorialThreeFragment extends Fragment {
+public class TutorialThreeFragment extends Fragment{
+    private View view;
+    private Button m_btn_start;
 
-//    Button button = getView().findViewById( R.id.startBtn );
-
-    public TutorialThreeFragment() {
-        // Required empty public constructor
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-
-//        Button button = getView().findViewById( R.id.startBtn );
-//        button.setOnClickListener( new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent( getActivity(), LoginActivity.class );
-//                startActivity( intent );
-//            }
-//        } );
     }
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate( R.layout.fragment_tutorial_three, container, false );
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate( R.layout.fragment_tutorial_three, container, false );
+        m_btn_start = view.findViewById(R.id.btn_start);
+        startLoginActivity();
+
+        return view;
     }
 
 
-
-
-
+    private void startLoginActivity() {
+        m_btn_start.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent( getActivity(), LoginActivity.class ));
+            }
+        });
+    }
 }
