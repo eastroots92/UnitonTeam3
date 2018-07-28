f7f21bb381b6bfc26de7bba0281f2368990ad145
Eastroots92 <eastroots92@gmail.com>
2018-07-28 19:05:01 +0900

    Fix conflict

---
+++ app/src/main/java/com/team3/uniton/unitonapplication/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.team3.uniton.unitonapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        init();
+    }
+
+    private void init() {
+
+    }
+}
--- app/src/main/java/com/team3/uniton/unitonapplication/MainActivity.java
+++ app/src/main/java/com/team3/uniton/unitonapplication/MainActivity.java
@@ -1,21 +1,18 @@
 package com.team3.uniton.unitonapplication;
 
-
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        init();
-    }
+    init();
+  }
 
-    private void init() {
-    }
+  private void init() {
+  }
 }
